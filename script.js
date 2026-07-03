80ac6ff6d8d499aea600d6f3488b4571a445cee8
rrowrrow <rrowiasrur@gmail.com>
2026-07-03 18:03:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const STORAGE_KEY = "katga_beta_v1";
-const DATA_URL = "./data/daily-k3.json";
-const WORD_PATH = "./data/";
+const DATA_URL = "./data/config.json";
+const WORD_PATH = "./data/daily/";
 
 const state = {
   config: null,
